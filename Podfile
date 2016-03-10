966bae6115fabdc5c109c2218eee0bdc815be4fd
itjhdev <github@itjh.com.cn>
2016-03-10 19:34:55 +0800

    添加cocoapods管理

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+use_frameworks!
+
+
+def mh_ios 
+    
+    pod 'Kingfisher'
+
+end
+
+target 'mh'  do
+mh_ios
+end
---
+++ Pods/Target Support Files/Pods-mh/Pods-mh-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_mhVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_mhVersionString[];
+
