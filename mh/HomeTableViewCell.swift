8bc2c9351570096b8bc3677ec12c0ab352aa26ff
itjhdev <github@itjh.com.cn>
2016-03-13 18:24:09 +0800

    编写首页页面

---
+++ mh/HomeTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  HomeTableViewCell.swift
+//  mh
+//
+//  Created by 宋立君 on 16/3/11.
+//  Copyright © 2016年 itjh. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+}
+
+extension HomeTableViewCell : UICollectionViewDataSource{
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("movieCell", forIndexPath: indexPath) as! MovieCollectionViewCell
+        
+//        cell.movieImage.image = UIImage(named: "img\(indexPath.row)")
+        
+        return cell
+    }
+    
+}
+
+
+extension HomeTableViewCell : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let itemsPerRow:CGFloat = 4
+        let hardCodedPadding:CGFloat = 5
+//        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+        
+    
+        
+        let itemWidth:CGFloat = 124
+
+        
+//        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+                let itemHeight = collectionView.bounds.height
+
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+}
---
+++ mh/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  mh
+//
+//  Created by 宋立君 on 16/3/11.
+//  Copyright © 2016年 itjh. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    @IBOutlet weak var movieName: UILabel!
+    
+    @IBOutlet weak var cacegoryName: UILabel!
+}
