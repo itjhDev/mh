1bb19426ab42970c0696bf5e8041234f82b16a5e
itjhdev <github@itjh.com.cn>
2016-04-01 17:25:39 +0800

    bonding movieInfo page

--- mh/MovieInfoViewController.swift
+++ mh/MovieInfoViewController.swift
@@ -14,11 +14,25 @@ class MovieInfoViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        self.navigationController?.navigationBar.backgroundColor = UIColor.blackColor()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+
     }
     
 
